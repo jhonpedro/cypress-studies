11ec552d916f6086ed04714ebff56a8f67bc14ff
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2022-06-27 13:52:33 -0300

    feat: base todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { Todo } from './apps/Todo'
 
 function App() {
-	return <div className='App'>React app</div>
+	const getCurrentApp = (): React.ReactNode => {
+		const params = Object.fromEntries(
+			new URLSearchParams(window.location.search)
+		)
+
+		const currentAppQueryParam = params.app
+
+		switch (currentAppQueryParam) {
+			case 'todo':
+				return <Todo />
+			default:
+				return <span>Not valid app</span>
+		}
+	}
+
+	return <div>{getCurrentApp()}</div>
 }
 
 export default App
---
+++ src/apps/Todo.tsx
@@ -0,0 +1,68 @@
+import React, { useRef, useState } from 'react'
+
+export const Todo = () => {
+	const newTodoInputRef = useRef<HTMLInputElement>(null)
+	const [todos, setTodos] = useState<Array<{ label: string; done: boolean }>>(
+		[]
+	)
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault()
+
+		const newTodoLabel = newTodoInputRef.current?.value
+
+		if (!newTodoLabel) {
+			return
+		}
+
+		setTodos((prevState) => [
+			...prevState,
+			{ done: false, label: newTodoLabel },
+		])
+
+		newTodoInputRef.current.value = ''
+	}
+
+	const handleDoneTodo = (todoName: string) => {
+		const currentTodoPosition = todos.findIndex(
+			({ label }) => label === todoName
+		)
+
+		setTodos((prevState) =>
+			prevState.map((todo, index) => {
+				if (index === currentTodoPosition) {
+					return { ...todo, done: !todo.done }
+				}
+
+				return todo
+			})
+		)
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='What do we need to do?'
+					ref={newTodoInputRef}
+				/>
+			</form>
+
+			<div>
+				<ul>
+					{todos.length === 0 && <span>Nothing to do</span>}
+					{todos.map((todo) => (
+						<li key={todo.label}>
+							<input
+								type='checkbox'
+								onClick={() => handleDoneTodo(todo.label)}
+							/>
+							{todo.label}
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	)
+}
